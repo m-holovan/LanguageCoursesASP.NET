e1e11c459f60da06210840d05a33449367d6e9da
m-holovan <nikita2003n@gmail.com>
2023-08-31 23:21:25 +0300

    Add teachers controller

---
+++ Controllers/TeacherController.cs
@@ -0,0 +1,20 @@
+﻿using LanguageCourses.Data;
+using LanguageCourses.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageCourses.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly ITeacherRepository _teacherRepository;
+        public TeacherController(ITeacherRepository teacherRepository)
+        {
+            _teacherRepository = teacherRepository;
+        }
+        public IActionResult Index()
+        {
+            var teachers = _teacherRepository.GetAllTeachers();
+            return View(teachers);
+        }
+    }
+}
