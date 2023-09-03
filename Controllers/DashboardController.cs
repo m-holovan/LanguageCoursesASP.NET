d572508415b5d8466bd239a941da9cfdaf1e728c
m-holovan <nikita2003n@gmail.com>
2023-09-03 23:19:08 +0300

    add dashboard controller and view

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageCourses.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
