1234c3f5c3eeb3f8b240d54176e87515ac7e789f
m-holovan <nikita2003n@gmail.com>
2023-09-03 15:01:48 +0300

    Add login method and view

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using LanguageCourses.Data;
+using LanguageCourses.Models;
+using LanguageCourses.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageCourses.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginVM);
+            }
+
+            var user = await _userManager.FindByEmailAsync(loginVM.Email);
+
+            if (user != null)
+            {
+                //User was find, now we check password
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+
+                if (passwordCheck)
+                {
+                    //In case if password was correct
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Course");
+                    }
+                }
+                //In case if password incorrect
+                TempData["Error"] = "Wrong credentials. Please try again.";
+                return View(loginVM);
+            }
+            //if user with this email was not found
+            TempData["Error"] = "Wrong credentials. Please try again.";
+            return View(loginVM);
+        }
+    }
+}
