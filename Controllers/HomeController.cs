3c483541d1e9db17aedd4dfdd06cc3aec2127244
m-holovan <nikita2003n@gmail.com>
2023-09-09 17:49:32 +0300

    Add ip geolocation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,56 @@
-﻿using LanguageCourses.Models;
+﻿using LanguageCourses.Helpers;
+using LanguageCourses.Interfaces;
+using LanguageCourses.Models;
+using LanguageCourses.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Globalization;
+using System.Net;
 
 namespace LanguageCourses.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ICourseRepository _courseRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICourseRepository courseRepository)
         {
             _logger = logger;
+            _courseRepository = courseRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var ipInfo = new IpInfo();
+            var homeViewModel = new HomeViewModel();
+
+            try
+            {
+                string url = "https://ipinfo.io?token=2aae7c726a664e";
+                var info = new WebClient().DownloadString(url);
+                ipInfo = JsonConvert.DeserializeObject<IpInfo>(info);
+                RegionInfo regInfo = new RegionInfo(ipInfo.Country);
+                ipInfo.Country = regInfo.EnglishName;
+                homeViewModel.City = ipInfo.City;
+                homeViewModel.State = ipInfo.Region;
+
+                if (homeViewModel != null)
+                {
+                    homeViewModel.Courses = await _courseRepository.GetCourseByCity(homeViewModel.City);
+                }
+                else
+                {
+                    homeViewModel.Courses = null;
+                }
+                return View(homeViewModel);
+            }
+            catch (Exception ex)
+            {
+                homeViewModel.Courses = null;
+            }
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using LanguageCourses.Models;
+
+namespace LanguageCourses.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Course>? Courses { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+    }
+}
