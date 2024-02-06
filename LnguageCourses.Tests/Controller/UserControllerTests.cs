c17f3ae9f8d150d42e59b2a0be2ee0b5048280a3
m-holovan <nikita2003n@gmail.com>
2024-02-06 16:03:13 +0200

    Added test for user`s controller and small changes in design

---
+++ LnguageCourses.Tests/Controller/UserControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using LanguageCourses.Controllers;
+using LanguageCourses.Interfaces;
+using LanguageCourses.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageCourses.Tests.Controller
+{
+    public class UserControllerTests
+    {
+        private IUserRepository _userRepository;
+        private IHttpContextAccessor _contextAccessor;
+        private UserController _userController;
+
+        public UserControllerTests()
+        {
+            _userRepository = A.Fake<IUserRepository>();
+            _contextAccessor = A.Fake<IHttpContextAccessor>();
+
+            _userController = new UserController(_userRepository, _contextAccessor);
+        }
+
+        [Fact]
+        public void UserController_Index_ReturnSuccess()
+        {
+            var id = _contextAccessor.HttpContext.User.GetUserId();
+            var user = A.Fake<User>();
+
+            A.CallTo(() => _userRepository.GetUserById(id)).Returns(user);
+
+            var result = _userController.Index();
+
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+
+        [Fact]
+        public void UserController_Detail_ReturnSuccess()
+        {
+            var id = _contextAccessor.HttpContext.User.GetUserId();
+            var user = A.Fake<User>();
+
+            A.CallTo(() => _userRepository.GetUserById(id)).Returns(user);
+
+            var result = _userController.Detail(id);
+
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+    }
+}
